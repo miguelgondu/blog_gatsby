f368471a7c693f7c68a1bd6f0ecf596ccd791ae2
Miguel González Duque <miguelgondu@gmail.com>
2020-09-26 17:15:49 +0200

    Removes a fragment from the spanish index

--- src/pages/es.js
+++ src/pages/es.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react"
+import React from "react"
 import { graphql } from "gatsby"
 import LayoutEs from "../components/layoutEs"
 import Blogpost from "../components/blogpost"
