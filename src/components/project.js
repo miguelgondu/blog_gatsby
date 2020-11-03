fc60f8fcd0e76ab68c32d5315b8d2d807fbb0e29
Miguel Gonzalez Duque <migd@itu.dk>
2020-11-03 10:09:11 +0100

    Adds links to projects in their titles

--- src/components/project.js
+++ src/components/project.js
@@ -4,7 +4,7 @@ import { Link } from "gatsby"
 export default ( props ) => (
     <div>
         <Link to={props.linkto}><img class="project" src={props.imgsrc} alt=""></img></Link>
-        <h2>{props.title}</h2>
+        <h2><Link to={props.linkto}>{props.title}</Link></h2>
         <p>{props.children}</p>
         <hr></hr>
     </div>
